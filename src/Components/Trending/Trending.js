7bd2872cc44e3bb7b655e601687a754b0d272ccf
Krutika Jichkar <93243210+krutikajichkar@users.noreply.github.com>
2023-03-09 10:49:56 +0530

    bug fixed

--- src/Components/Trending/Trending.js
+++ src/Components/Trending/Trending.js
@@ -12,7 +12,7 @@ function Trending() {
 
   const fetchResults = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}=${page}`
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}${page}`
     );
 
     setContent(data.results);
